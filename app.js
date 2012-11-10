ec40347ff5a7586f3e57479714288b10a9ce174a
Izzy Johnston <izzycjohnston@gmail.com>
2012-11-10 16:31:04 -0500

    Adding in Open Dover

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ Penguin.Article = function(article_id){
        dataType: 'jsonp',
        jsonp: 'jsonp',
        success: function(json){
-           console.log(Penguin.FirstTwoParagraphs(json.article.content))
+           OpenDover.SearchSentiments(Penguin.FirstTwoParagraphs(json.article.content))
        },
        error:function(e){
            console.log(e)
@@ -72,8 +72,31 @@ Penguin.FirstTwoParagraphs = function(content){
     }
     return paragraphs;
 }
+
+OpenDover = {}
+
+OpenDover.SearchSentiments = function(text){
+    $.ajax({
+       type: 'GET',
+       url: OpenDover.Url + 'search_sentiments/',
+       data: {
+           apikey: OpenDover.ApiKey,
+           text: text
+       },
+       dataType: 'jsonp',
+       jsonp: 'jsonp',
+       success: function(json){
+           console.log('json')
+       },
+       error:function(e){
+           console.log(e)
+       }
+    });
+}
 $(function(){
     Penguin.ApiKey = '36fc10e2f13c5935b01566ece096e394';
     Penguin.Url = 'https://api.pearson.com/penguin/classics/v1/';
+    OpenDover.ApiKey = 't4v8ga8xg5tkm8b5zya8833s';
+    OpenDover.Url = ' http://api.opendover.nl/rest/v1/json/';
     Penguin.Books();
 });
